bc38e86109c23ef7eaf1757e391fe532ab4351d5
OttoKorhonen <60289527+OttoKorhonen@users.noreply.github.com>
2020-04-18 11:49:17 +0300

    Security kommentoitu pois

--- vegetablewholesale/src/main/java/hh/swd20/vegetablewholesale/WebSecurityConfig.java
+++ vegetablewholesale/src/main/java/hh/swd20/vegetablewholesale/WebSecurityConfig.java
@@ -1,47 +1,47 @@
-package hh.swd20.vegetablewholesale;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import hh.swd20.vegetablewholesale.web.UserDetailServiceImpl;
-
-@Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-	
-	@Autowired
-	private UserDetailServiceImpl userDetailService;
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		http
-		.authorizeRequests().antMatchers("/css/**","/api/products","/products","/categories","/api/categories","/products/{productId}").permitAll()
-		.antMatchers("/", "/").permitAll()//kuka tahansa käyttäjä voi nähdä productlist sivun
-        .and()
-        .authorizeRequests()
-        .antMatchers("/", "/productlist").permitAll()//kuka tahansa käyttäjä voi nähdä productlist sivun
-        .antMatchers("/delete/{productId}").hasRole("ADMIN")//ainoastaan admin-oikeudet omaava käyttäjä voi poistaa tuotteen
-        .antMatchers("/editproduct/{productId}").hasRole("ADMIN")//ainoastaan admin-oikeudet omaava käyttäjä voi poistaa tuotteen
-        .and()
-      .formLogin()
-          .loginPage("/login")
-          .defaultSuccessUrl("/productlist")
-          .permitAll()
-          .and()
-      .logout()
-          .permitAll();
-	}
-	
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-		auth.userDetailsService(userDetailService).passwordEncoder(new BCryptPasswordEncoder());
-	}
-
-}
+//package hh.swd20.vegetablewholesale;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//import hh.swd20.vegetablewholesale.web.UserDetailServiceImpl;
+//
+//@Configuration
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+//	
+//	@Autowired
+//	private UserDetailServiceImpl userDetailService;
+//	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception{
+//		http
+//		.authorizeRequests().antMatchers("/css/**","/api/products","/products","/categories","/api/categories","/products/{productId}").permitAll()
+//		.antMatchers("/", "/").permitAll()//kuka tahansa käyttäjä voi nähdä productlist sivun
+//        .and()
+//        .authorizeRequests()
+//        .antMatchers("/", "/productlist").permitAll()//kuka tahansa käyttäjä voi nähdä productlist sivun
+//        .antMatchers("/delete/{productId}").hasRole("ADMIN")//ainoastaan admin-oikeudet omaava käyttäjä voi poistaa tuotteen
+//        .antMatchers("/editproduct/{productId}").hasRole("ADMIN")//ainoastaan admin-oikeudet omaava käyttäjä voi poistaa tuotteen
+//        .and()
+//      .formLogin()
+//          .loginPage("/login")
+//          .defaultSuccessUrl("/productlist")
+//          .permitAll()
+//          .and()
+//      .logout()
+//          .permitAll();
+//	}
+//	
+//	@Autowired
+//	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+//		auth.userDetailsService(userDetailService).passwordEncoder(new BCryptPasswordEncoder());
+//	}
+//
+//}

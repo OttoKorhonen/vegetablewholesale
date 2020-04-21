ee7e1efe8e2a412a8cea8e0ced27ff4ded5eb2e0
OttoKorhonen <60289527+OttoKorhonen@users.noreply.github.com>
2020-04-21 17:55:31 +0300

    Testit kommentoitu pois

--- vegetablewholesale/src/test/java/hh/swd20/vegetablewholesale/VegetablewholesaleApplicationTests.java
+++ vegetablewholesale/src/test/java/hh/swd20/vegetablewholesale/VegetablewholesaleApplicationTests.java
@@ -1,25 +1,25 @@
-package hh.swd20.vegetablewholesale;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import hh.swd20.vegetablewholesale.web.ProductController;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-class VegetablewholesaleApplicationTests {
-
-	@Autowired
-	private ProductController controller;
-	
-	@Test
-	void contextLoads() throws Exception{
-		assertThat(controller).isNotNull();
-	}
-
-}
+//package hh.swd20.vegetablewholesale;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import hh.swd20.vegetablewholesale.web.ProductController;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//class VegetablewholesaleApplicationTests {
+//
+//	@Autowired
+//	private ProductController controller;
+//	
+//	@Test
+//	void contextLoads() throws Exception{
+//		assertThat(controller).isNotNull();
+//	}
+//
+//}
